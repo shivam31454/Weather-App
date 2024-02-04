6568c7e32135fba92d14c6198258805d1ecf63ef
Shivam Singh <145860209+shivam31454@users.noreply.github.com>
2024-02-04 10:52:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ async function checkWeather(city){
         document.querySelector(".weather").style.display="none";
     }else{
         var data=await response.json();
-    // console.log(data);
-
     document.querySelector(".city").innerHTML=data.name;
     document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
     document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
